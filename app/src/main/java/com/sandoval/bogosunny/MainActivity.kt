184a7d2a6065afeb5fc8c587604774f6b7940d7e
juan.sandoval <Naruto2790>
2019-12-09 14:50:07 -0500

    activity_base design branch: decouple activity from base activity  * Added Theme validation to Base Activity  * Extending base activity in main activity

--- app/src/main/java/com/sandoval/bogosunny/MainActivity.kt
+++ app/src/main/java/com/sandoval/bogosunny/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.sandoval.bogosunny
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
+import com.sandoval.bogosunny.ui.BaseActivity
+import com.sandoval.bogosunny.utils.ThemeUtils
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.toolbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = ""
+        if (ThemeUtils.isNight())
+            changeRings()
+    }
+
+    private fun changeRings() {
+        rings.setBackgroundResource(R.drawable.circular_rings_dark)
+        sun_and_moon.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#FFFFFF"))
+        add_city_button.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#403C48"))
+
     }
 }
