f922b609d2bc5bab0bba99158c80bfc624e151f3
juan.sandoval <Naruto2790>
2019-12-10 08:46:36 -0500

    activity_base design branch: decouple activity from base activity  * Fixed typo

--- app/src/main/java/com/sandoval/bogosunny/ui/BaseActivity.kt
+++ app/src/main/java/com/sandoval/bogosunny/ui/BaseActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.sandoval.bogosunny.utils.ThemeUtils
 
 open class BaseActivity : AppCompatActivity() {
-    private var progrssDialog: Dialog? = null
+    private var progressDialog: Dialog? = null
 
     override fun getTheme(): Resources.Theme {
         val theme = super.getTheme()
