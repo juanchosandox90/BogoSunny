5063e9d10c7f1a27433e38192c3fc8b213f9e8c6
juan.sandoval <Naruto2790>
2019-12-11 11:36:56 -0500

    dev_location_permissions  * Added location permissions  * Added validation to activate location  * Added new colors  * check validation of permissions just once and active location

--- app/src/main/java/com/sandoval/bogosunny/ui/base/BaseActivity.kt
+++ app/src/main/java/com/sandoval/bogosunny/ui/base/BaseActivity.kt
@@ -1,9 +1,12 @@
 package com.sandoval.bogosunny.ui.base
 
 import android.app.Dialog
+import android.content.pm.PackageManager
 import android.content.res.Resources
+import android.os.Build
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.sandoval.bogosunny.utils.CommonUtils
 import com.sandoval.bogosunny.utils.ThemeUtils
 
@@ -13,6 +16,20 @@ open class BaseActivity : AppCompatActivity() {
     override fun getTheme(): Resources.Theme {
         val theme = super.getTheme()
         return ThemeUtils.getTheme(theme)
+    }
+
+    fun hasPermissions(permissions: Array<String>): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return true
+        return permissions.none {
+            ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    fun requestPermissionsSafely(permissions: Array<String>, requestCode: Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(permissions, requestCode)
+        }
     }
 
     fun showLoading() {
--- app/src/main/java/com/sandoval/bogosunny/utils/AppConstants.kt
+++ app/src/main/java/com/sandoval/bogosunny/utils/AppConstants.kt
@@ -1,2 +1,6 @@
 package com.sandoval.bogosunny.utils
 
+object AppConstants {
+    val REQUEST_CHECK_SETTINGS = 888
+    val LOCATION_PERMISSION_REQUEST = 999
+}
